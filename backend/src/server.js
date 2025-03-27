0c921d706044378d067af55555072d4dfa7b4d19
Abhigyanpaul1234 <92012565+Abhigyanpaul1234@users.noreply.github.com>
2025-03-27 11:48:55 +0530

    testing workflow

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,7 @@ mongoose.connect(process.env.MONGO_URI, {
 
 
 app.get("/", (req, res) => {
-  res.send("AI Mock Interview System API is running...");
+  res.send("AI Mock Interview System API is running..");
 });
 
 
